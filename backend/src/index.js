25bca261eb191ed775eeec05617543b83b358559
Josh Bernstein <joshua.bernstein@outlook.com>
2019-06-16 16:51:28 -0600

    Finished video 12

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,21 @@
-// let's go!
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+
+//TODO Use express middleware to handle cookies(JWT)
+//TODO Use express middleware to populate current user
+//!Look into what happens with PLAYGROUND_URL on the server
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: [process.env.FRONTEND_URL,process.env.PLAYGROUND_URL]
+    }
+  },
+  details => {
+    console.log(`Server is no running on port https:/localhost:${details.port}`);
+  }
+);
