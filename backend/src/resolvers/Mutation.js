8de395d51c56cea4e2a66a31736bd3c8d7e806d9
Josh Bernstein <joshua.bernstein@outlook.com>
2019-06-20 23:37:12 -0600

    Added signup mutation to backend

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,8 +1,9 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     //TODO: Check if they are logged in
-    console.log(args);
-    console.log(ctx.db);
     const item = await ctx.db.mutation.createItem(
       {
         data: { ...args },
@@ -10,6 +11,59 @@ const Mutations = {
       info,
     );
     return item;
+  },
+
+  updateItem(parent, args, ctx, info) {
+    //First take a copy of the updates
+    const updates = { ...args };
+    //Remove the ID from the updates
+    delete updates.id;
+    //Run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id,
+        },
+      },
+      info,
+    );
+  },
+
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    //1. Find the item
+    const item = await ctx.db.query.item({ where }, `{id title}`);
+    //2. Check if they own the item, or have permissions
+    //TODO
+    //3. Delete
+    return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase();
+    //hash their password
+    const password = await bcrypt.hash(args.password, 12);
+    //create the user in the database
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] },
+        },
+      },
+      info,
+    );
+    //create the JWT token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    //Set the JWT as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    });
+
+    //Finally return the user to the browser
+    return user;
   },
 };
 
